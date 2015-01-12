53e7b3eec98729a611765b4ba017742dfa0c2592
atat <ana.tomboulian@gmail.com>
2015-01-11 18:42:31 -0700

    add FileJsonifier class to clean up logic

---
+++ filejsonifier.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import json
+import glob
+import sys
+import os
+import fileinput
+import re
+
+class FileJsonifier:
+  """JSONifies given pdf or csv"""
+
+  def __init__(self, file_path, ofp, index_type, index_count):
+    if file_path.lower().endswith('.csv'):
+      self.file_type = 'csv'
+    else:
+      #TODO: Make sure this doesn't always assume it's a pdf
+      self.file_type = 'pdf'
+
+    self.file_name = os.path.basename(file_path)
+    self.ifp = open(file_path)
+    self.ofp = ofp
+    self.index_type = index_type
+    self.index_count = index_count
+
+
+  def jsonify(self):
+    for line in self.ifp:
+      date = re.search('^[0-9]{2}\/[0-9]{2}\s+', line).group(0).rstrip()
+      date = self.file_name[:4] + '-' + date.replace("/", "-")
+      subject = re.search('(?![0-9]*\/)(?![0-9])(?!\s+).*\s(?=\$?[0-9,]+\.[0-9]{2})', line)
+      subject = subject.group(0)
+      subject = subject.rstrip()
+      value = re.search('[0-9,]+\.[0-9]{2}', line).group(0).replace(",","")
+
+      json.dump({"index":{"_index": "transactions","_type": self.index_type, "_id": self.index_count}}, self.ofp)
+      self.ofp.write('\n')
+      json.dump({"date": date, "subject": subject, "value": value}, self.ofp)
+      self.ofp.write('\n')
+
+      self.index_count += 1
+
+    print self.file_name, "complete."
+    self.ifp.close()
+
+  def __jsonify_csv():
+    print "good stuff"
--- json_maker.py
+++ json_maker.py
@@ -7,6 +7,7 @@ import os
 import getopt
 import fileinput
 import re
+from filejsonifier import FileJsonifier
 
 def clean_jsonified_dir():
   if not os.path.exists('jsonified'):
@@ -15,61 +16,70 @@ def clean_jsonified_dir():
     print "Removing ./jsonified/transactions.json"
     os.remove('./jsonified/transactions.json')
 
-def main(argv):
-
-  inputdir = '' #'./textified/'
-  try:
-    opts, args = getopt.getopt(argv,"hi:o:",["idir="])
-  except getopt.GetoptError:
-    print "json_maker -i <directory>"
-    sys.exit(2)
-  for opt, arg in opts:
-    if opt == '-h':
-      print "json_maker -i <directory>"
-      sys.exit()
-    elif opt in ("-i", "--idir"):
-      inputdir = arg
-
-  print 'Input directory is "',inputdir,'"'
-  clean_jsonified_dir()
-  output_filename = './jsonified/transactions.json'
-  fp = open(output_filename, 'w')
-
+def jsonify_from_inputdir(inputdir,ofp):
   if not os.path.exists(inputdir):
     print 'Directory "',inputdir,'" does not exist. Exiting.'
 
   print "Processing text entries into JSON format."
   index_count = 0
   for bank_statement in os.listdir(inputdir):
-    inputfile = open(inputdir + bank_statement, 'r')
     if bank_statement.endswith('deposits'):
       index_type = "deposits"
     elif bank_statement.endswith('withdrawals'):
       index_type = "withdrawals"
+    elif bank_statement.endswith('e_withdrawals'):
+      index_type = "e_withdrawals"
 
+    fj = FileJsonifier(inputdir + bank_statement, ofp, index_type, index_count)
+    fj.jsonify()
 
-    for line in inputfile:
-      date = re.search('^[0-9]{2}\/[0-9]{2}\s+', line).group(0).rstrip()
-      date = bank_statement[:4] + '-' + date.replace("/", "-")
-      subject = re.search('(?![0-9]*\/)(?![0-9])(?!\s+).*\s(?=\$?[0-9,]+\.[0-9]{2})', line)
-      subject = subject.group(0)
-      subject = subject.rstrip()
-      value = re.search('[0-9,]+\.[0-9]{2}', line).group(0).replace(",","")
+    index_count = fj.index_count
+    ofp = fj.ofp
+    del fj
 
-      json.dump({"index":{"_index": "transactions","_type": index_type, "_id": index_count}}, fp)
-      fp.write('\n')
-      json.dump({"date": date, "subject": subject, "value": value}, fp)
-      fp.write('\n')
+  ofp.close()
 
-      index_count += 1
+def usage():
+    print "json_maker -d <directory>"
+    print "OR"
+    print "json_maker -f <csvinputfile>"
 
-    print bank_statement, "complete."
-    inputfile.close()
+def main(argv):
 
-  fp.close()
+  inputdir = '' #'./textified/'
+  inputfile = '' #'./JMPC.csv'
+  try:
+    opts, args = getopt.getopt(argv,"hd:o:f:",["idir=","ifile="])
+  except getopt.GetoptError as err:
+    # print something like "option -a not recognized"
+    print str(err)
+    usage()
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      usage()
+      sys.exit()
+    elif opt in ("-d", "--idir"):
+      inputdir = arg
+    elif opt in ("-f", "--ifile"):
+      inputfile = arg
+    else:
+      assert False, "unhandled option"
+
+  if inputdir:
+    print "Input directory is {}".format(inputdir)
+  elif inputfile:
+    print "Input file is {}".format(inputfile)
+
+  clean_jsonified_dir()
+  output_filename = './jsonified/transactions.json'
+  ofp = open(output_filename, 'w')
+
+  jsonify_from_inputdir(inputdir,ofp)
 
   if os.path.exists(output_filename):
-    print output_filename, "successfully created!"
+    print output_filename, "successfully created."
+
 if __name__ == "__main__":
   main(sys.argv[1:])
 
